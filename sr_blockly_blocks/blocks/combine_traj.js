74a4ff227d249653a53a645f3c133b1157158e18
Ugo Cupcic <ugo@shadowrobot.com>
2016-08-29 16:48:44 +0200

    better combine traj

--- sr_blockly_blocks/blocks/combine_traj.js
+++ sr_blockly_blocks/blocks/combine_traj.js
@@ -1,14 +1,17 @@
 Blockly.Blocks['combine_traj'] = {
   init: function() {
-    this.appendValueInput("NAME")
+    this.appendValueInput("traj1")
         .setCheck("Trajectory")
         .appendField("Combine")
         .appendField("traj1");
-    this.appendValueInput("NAME")
+    this.appendValueInput("traj2")
         .setCheck("Trajectory")
         .appendField("traj2");
+    this.appendDummyInput()
+        .appendField("start together")
+        .appendField(new Blockly.FieldCheckbox("TRUE"), "sync");
     this.setInputsInline(true);
-    this.setOutput(true, "Trajectory");
+    this.setOutput(true, null);
     this.setColour(0);
     this.setTooltip('');
     this.setHelpUrl('http://www.example.com/');
--- sr_blockly_blocks/generators/python/combine_traj.js
+++ sr_blockly_blocks/generators/python/combine_traj.js
@@ -1,7 +1,9 @@
-Blockly.Python['combine_traj'] = function(block) {
-  var value_name = Blockly.Python.valueToCode(block, 'NAME', Blockly.Python.ORDER_ATOMIC);
-  var value_name = Blockly.Python.valueToCode(block, 'NAME', Blockly.Python.ORDER_ATOMIC);
-  // TODO: Assemble Python into code variable.
+Blockly.JavaScript['combine_traj'] = function(block) {
+  var value_traj1 = Blockly.JavaScript.valueToCode(block, 'traj1', Blockly.JavaScript.ORDER_ATOMIC);
+  var value_traj2 = Blockly.JavaScript.valueToCode(block, 'traj2', Blockly.JavaScript.ORDER_ATOMIC);
+  var checkbox_sync = block.getFieldValue('sync') == 'TRUE';
+  // TODO: Assemble JavaScript into code variable.
   var code = '...';
-  return code;
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.JavaScript.ORDER_NONE];
 };
